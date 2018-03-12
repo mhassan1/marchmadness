a86105fef56bc632125db4bc5195636453dd4951
Marc Hassan <marc.j.hassan@gmail.com>
2018-03-12 02:11:25 +0000

    teams model sort

--- application/models/Teams_model.php
+++ application/models/Teams_model.php
@@ -3,7 +3,7 @@
 class Teams_model extends CI_Model {
 
 	function get() {
-		$query = $this -> db -> query('SELECT * from madness_msnbc_teams');
+		$query = $this -> db -> query("SELECT * from madness_msnbc_teams where team_name!='' order by team_name");
 
 		$a = array();
 		foreach ($query->result() as $row) {
