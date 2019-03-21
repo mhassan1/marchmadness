041b53320169097c2bf8eec7ed7d7641d552ae14
Marc Hassan <marc.j.hassan@gmail.com>
2019-03-20 21:04:17 -0400

    fix standings model to match style storage

--- application/models/Standings_model.php
+++ application/models/Standings_model.php
@@ -4,8 +4,8 @@ class Standings_model extends CI_Model {
 
 	function get() {
 		$query = $this -> db -> query('select a.username,'
-			.'sum(if(a.format3=\'color:green;\',10*pow(2,a.round+1),0)) points,'
-			.'sum(if(a.format3 in (\'color:green;\',\'color:black;\'),10*pow(2,a.round+1),0)) potential,'
+			.'sum(if(a.format3=\'{"color":"green"}\',10*pow(2,a.round+1),0)) points,'
+			.'sum(if(a.format3 in (\'{"color":"green"}\',\'{"color":"black"}\'),10*pow(2,a.round+1),0)) potential,'
 			.'max(if(a.bracket_id=127,a.team_name,null)) finalpick, '
 			.'max(if(a.bracket_id=127,a.format3,null)) finalpickformat3 '
 			.'from madness_format3_view a, '
