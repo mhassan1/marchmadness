b0feef0cf294c5c198e84b1eb75270edbac60334
mhassan1 <marc.j.hassan@gmail.com>
2024-03-31 21:21:28 -0400

    handle undefined `events` in odds response

--- packages/express/src/updatePicksAndOdds.ts
+++ packages/express/src/updatePicksAndOdds.ts
@@ -123,7 +123,7 @@ export const oddsUpdate = async () => {
       data: {
         sports: [
           {
-            leagues: [{ events }],
+            leagues: [{ events = [] }],
           },
         ],
       },
